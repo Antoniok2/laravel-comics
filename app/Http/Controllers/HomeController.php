b2afb84e73a1b80de8191e7052194556bd241b31
Antoniok2 <antoniopiazza18@gmail.com>
2022-01-25 16:51:22 +0100

    other method controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,18 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
+    private $menu = ['character', 'comics', 'movie'];
+
     public function test()
     {
-        return view('views.home');
+        $data = ['menu' => $this -> menu];
+        return view('views.home', $data);
+        // return view('views.home', compact('menu')); secondo modo
     }
 
     public function detail() 
     {
-        return view('views.detail');
+        $menu = $this ->menu;
+        return view('views.detail', compact('menu'));
     }
 }
